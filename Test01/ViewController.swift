e4ce44c5382cf577d3858466da95addf050468ec
Dominic Go <dominicstop@gmail.com>
2020-08-27 09:00:47 +0800

    Initial Commit

---
+++ Test01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test01
+//
+//  Created by Dominic Go on 8/27/20.
+//  Copyright © 2020 Dominic Go. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
